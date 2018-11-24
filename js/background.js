5a7b6d7df99055e6296996b83163e7c5179f890d
digitorum <me@digitorum.ru>
2018-11-25 02:57:52 +0300

    Загрузка данных с гитхаба.

--- js/background.js
+++ js/background.js
@@ -1,13 +1,45 @@
 'use strict';
 
+/**
+ * Словарь со списком стэндов.
+ * 
+ * @type {object}
+ */
 let stands = {};
 
-const observeStandsRequest = (e) => {
-  if (e.currentTarget.readyState === XMLHttpRequest.DONE) {
-    stands = e.currentTarget.response;
-  }
+/**
+ * Загрузить спракочник стэндов
+ * 
+ * @param {string} url
+ * @returns {Promise} 
+ */
+const loadStandsData = (url) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = (e) => {
+      if (e.currentTarget.readyState === XMLHttpRequest.DONE) {
+        if (e.currentTarget.response) {
+          resolve(e.currentTarget.response);
+        } else {
+          reject();
+        }
+      };
+    }
+    xhr.onerror = reject;
+    xhr.onabort = reject;
+    xhr.responseType = 'json';
+    xhr.open('GET', url, true);
+    xhr.send();
+  })
 }
 
+/**
+ * Разобрать вводимое пользователем значение для получение бранча и домена проекта.
+ * 
+ * @param {string} text 
+ * @returns {Promise}
+ */
 const parseQuery = (text) => {
   return new Promise((resolve, reject) => {
     let chunks = text.split(/ /ig).filter(chunk => !!chunk);
@@ -19,24 +51,30 @@ const parseQuery = (text) => {
   });
 };
 
+/**
+ * Сформировать список подсказок
+ * 
+ * @param {object} qs
+ * @returns {Promise}
+ */
 const getSuggests = (qs) => {
-  let node;
+  return new Promise((resolve) => {
+    let node;
 
-  if (
-    qs.branch.match(/([0-9]+\-)+[0-9]+/)
-    || qs.branch == 'develop'
-    || qs.branch == 'master'
-  ) {
-    node = "";
-  } else {
-    let matches = qs.branch.match(/([a-z]+)\-[0-9]/);
+    if (
+      qs.branch.match(/([0-9]+\-)+[0-9]+/)
+      || qs.branch == 'develop'
+      || qs.branch == 'master'
+    ) {
+      node = "";
+    } else {
+      let matches = qs.branch.match(/([a-z]+)\-[0-9]/);
 
-    if (matches) {
-      node = matches[1].toUpperCase();
+      if (matches) {
+        node = matches[1].toUpperCase();
+      }
     }
-  }
-  
-  return new Promise((resolve, reject) => {
+
     resolve(
       qs.query 
         ? Object.keys(stands)
@@ -58,15 +96,21 @@ const getSuggests = (qs) => {
                   }
                 }
 
-                return list.map((domain) => `https://${qs.branch}-${domain.replace(/\./g, '-')}.old.${project}.aservices.tech`);
+                return list.map((domain) => ({
+                  branch: qs.branch,
+                  domain: domain,
+                  project: project
+                }));
               })
               .reduce((a, b) => a.concat(b), [])
           })
           .reduce((a, b) => a.concat(b), [])
-          .map((domain) => {
+          .map((data) => {
+            const domain = data.domain.replace(/\./g, '-');
+
             return { 
-              content: domain, 
-              description: domain
+              content: `https://${data.branch}-${domain}.old.${data.project}.aservices.tech`, 
+              description: `<url>${data.branch}-${domain}.old.${data.project}.aservices.tech</url> - ${data.domain}`
             };
           })
         : []
@@ -74,18 +118,26 @@ const getSuggests = (qs) => {
   })
 }
 
-const xhr = new XMLHttpRequest();
-xhr.onreadystatechange = observeStandsRequest;
-xhr.responseType = "json";
-xhr.open("GET", chrome.extension.getURL('../data/stands.json'), true);
-xhr.send();
+// Пробуем получить копию данных с гитхаба.
+// Если не получилось, читаем локальный файл.
+loadStandsData('https://raw.githubusercontent.com/digitorum/stand-omnibox/master/data/stands.json').then((response) => {
+  stands = response;
+}, () => {
+  loadStandsData(chrome.extension.getURL('../data/stands.json')).then((response) => {
+    stands = response;
+  }, () => {
+    stands = {};
+  })
+});
 
+// Реакция на ввод пользователя.
 chrome.omnibox.onInputChanged.addListener((text, suggest) => {
   parseQuery(text)
     .then(getSuggests)
       .then(suggest);
 });
 
+// Реакция на выбор пользователя.
 chrome.omnibox.onInputEntered.addListener((text, sender) => {
   chrome.tabs.getSelected(null, (tab) => {
     chrome.tabs.update(tab.id, { url: text });
